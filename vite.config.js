1bf2a081532b92d1528f082d252101348cace86f
cabrera-evil <douglascabrera.dev@gmail.com>
2023-06-07 20:50:31 -0600

    Feature: docker

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  
+  // Additional Configuration Options
+  server: {
+    port: 80,
+    open: true,
+  },
+  
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    // Additional build options
+  },
+});
