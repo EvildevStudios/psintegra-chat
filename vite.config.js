311d199cb0afc1f7912b5fea4b0aa351a93a6af5
cabrera-evil <douglascabrera.dev@gmail.com>
2023-06-09 15:51:46 -0600

    Add: app manifest

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,4 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  
-  // Additional Configuration Options
-  server: {
-    port: 80,
-    open: true,
-  },
-  
-  build: {
-    outDir: 'dist',
-    sourcemap: true,
-    // Additional build options
-  },
 });
