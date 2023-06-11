a0952e00c5ea465f6b930205a5d0cb6fb04094e0
cabrera-evil <douglascabrera.dev@gmail.com>
2023-06-11 11:54:50 -0600

    Add: time elapsed function

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,15 +1,34 @@
-import React, { useContext, useEffect, useRef } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import { AuthContext } from "../context/AuthContext";
 import { ChatContext } from "../context/ChatContext";
 
 const Message = ({ message }) => {
     const { currentUser } = useContext(AuthContext);
     const { data } = useContext(ChatContext);
+    const [timeElapsed, setTimeElapsed] = useState("");
 
     const ref = useRef();
 
     useEffect(() => {
         ref.current?.scrollIntoView({ behavior: "smooth" });
+
+        // Calculate the time elapsed since the message was sent
+        const messageTime = new Date(message.date).getTime();
+        const currentTime = new Date().getTime();
+        const elapsed = Math.floor((currentTime - messageTime) / 1000); // in seconds
+
+        if (elapsed < 60) {
+            setTimeElapsed(`${elapsed} seconds ago`);
+        } else if (elapsed < 3600) {
+            const minutes = Math.floor(elapsed / 60);
+            setTimeElapsed(`${minutes} minutes ago`);
+        } else if (elapsed < 86400) {
+            const hours = Math.floor(elapsed / 3600);
+            setTimeElapsed(`${hours} hours ago`);
+        } else {
+            const days = Math.floor(elapsed / 86400);
+            setTimeElapsed(`${days} days ago`);
+        }
     }, [message]);
 
     return (
@@ -26,7 +45,7 @@ const Message = ({ message }) => {
                     }
                     alt=""
                 />
-                <span>just now</span>
+                <span>{timeElapsed}</span>
             </div>
             <div className="messageContent">
                 <p>{message.text}</p>
